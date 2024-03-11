afc9e7eeb916ae8e8565f17582093c6f5e55c2b7
Naman nanda <64105119+nimo-codes@users.noreply.github.com>
2024-03-12 04:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, UploadFile, File
 import speech_recognition as sr
-import openai
-from typing import Optional
 
+import openai
+import io
+
+openai.api_key = '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'
 app = FastAPI()
 
-
-openai.api_key = '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'
-
 @app.post("/process_audio")
-async def process_audio(file: UploadFile = File(...), duration: Optional[int] = 5):
+async def process_audio(file: UploadFile = File(...)):
     try:
-        # Perform speech recognition on the uploaded WAV file
+        file_data = await file.read()
+        audio_data = io.BytesIO(file_data)
         recognizer = sr.Recognizer()
-        audio_data = sr.AudioFile(file.file)
-        with audio_data as source:
-            audio = recognizer.record(source, duration=duration)
+        with sr.AudioFile(audio_data) as source:
+            audio = recognizer.record(source)
         recognized_text = recognizer.recognize_google(audio)
-
         model_name = "gpt-3.5-turbo"
         temperature = 0.7
         max_tokens = 150
         prompt = f"Tell me only the medical details from the next paragraph (if there are no medical details in it, just say no medical details available and if available list those) and summarize it in brief 3 - 4 points,{recognized_text}"
-
         response = openai.ChatCompletion.create(
             model=model_name,
             messages=[{"role": "system", "content": prompt}],
@@ -33,6 +30,38 @@ async def process_audio(file: UploadFile = File(...), duration: Optional[int] = 
         )
 
         return {"summary": response.choices[0].message.content.strip()}
+
     except Exception as e:
+
         return {"error": str(e)}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# model_name = "gpt-3.5-turbo"
+#         temperature = 0.7
+#         max_tokens = 150
+#         prompt = f"Tell me only the medical details from the next paragraph (if there are no medical details in it, just say no medical details available and if available list those) and summarize it in brief 3 - 4 points,{recognized_text}"
+
+#         response = openai.ChatCompletion.create(
+#             model=model_name,
+#             messages=[{"role": "system", "content": prompt}],
+#             max_tokens=max_tokens,
+#             n=1,
+#             stop=None,
+#             temperature=temperature,
+#         )
+
+#         return {"summary": response.choices[0].message.content.strip()}
